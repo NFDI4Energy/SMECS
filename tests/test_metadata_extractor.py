043b8ef83b5f9a38d398b99f69d753e23c3915f1
Aida Jafarbigloo <aida.jafarbigloo.274@gmail.com>
2024-12-09 12:57:17 +0100

    Fix a bug

--- tests/test_metadata_extractor.py
+++ tests/test_metadata_extractor.py
@@ -24,10 +24,11 @@ class TestDataExtraction(unittest.TestCase):
         request = MagicMock(method='POST', POST={'gl_url': GitLab_url, 'personal_token_key': personal_token_gl})
         result = data_extraction(request)
         self.assertIsInstance(result, tuple)
-        self.assertEqual(len(result), 2)
-        metadata, context = result
+        self.assertEqual(len(result), 3)
+        metadata, context, hermes_metadata = result
         self.assertIsInstance(metadata, dict)
         self.assertIsInstance(context, dict)
+        self.assertIsInstance(hermes_metadata, dict) 
         self.assertIn('gl_url', context)
         self.assertEqual(context['gl_url'], GitLab_url)
 
@@ -39,10 +40,11 @@ class TestDataExtraction(unittest.TestCase):
         request = MagicMock(method='POST', POST={'gl_url': GitHub_url, 'personal_token_key': personal_token_gh})
         result = data_extraction(request)
         self.assertIsInstance(result, tuple)
-        self.assertEqual(len(result), 2)
-        metadata, context = result
+        self.assertEqual(len(result), 3)
+        metadata, context, hermes_metadata = result
         self.assertIsInstance(metadata, dict)
-        self.assertIsInstance(context, dict)   
+        self.assertIsInstance(context, dict)
+        self.assertIsInstance(hermes_metadata, dict) 
         self.assertIn('gl_url', context)
         self.assertEqual(context['gl_url'], GitHub_url)
 
