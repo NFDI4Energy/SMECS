c951fdceea2be7b3695b2f5f6827113c80c15eb1
sferenz <stephan.ferenz@uol.de>
2025-03-31 11:14:20 +0200

    "Cleaned up files and improved .gitignore"

--- meta_tool/settings.py
+++ meta_tool/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django_jsonforms',
-    'meta_show',
     'meta_creator',
     'django.contrib.staticfiles',
 ]
