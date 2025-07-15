8e757409ce8539040b8ec8485fcbf6471fd44f42
Sundraiz-Shah <sundraiz.shah@gmail.com>
2025-07-15 15:36:16 +0200

    pinkish text fields updates

--- static/foundation/js/vendor/init.js
+++ static/foundation/js/vendor/init.js
@@ -1,21 +1,20 @@
 // init.js
 
-import { setupForm } from './form-utils.js';
-import { initializeTaggingFields} from './tagging.js';
-import { setupTables} from './table-utils.js';
-import { setupDownload } from './download.js';
-import { setupUI, loadpage} from './ui.js';
-import{ initializeDynamicDropdowns } from './dropdown-utils.js';
-import {setMandatoryFieldsFromSchema} from './schema-utils.js';
+import { setupForm } from "./form-utils.js";
+import { initializeTaggingFields } from "./tagging.js";
+import { setupTables } from "./table-utils.js";
+import { setupDownload } from "./download.js";
+import { setupUI, loadpage } from "./ui.js";
+import { initializeDynamicDropdowns } from "./dropdown-utils.js";
+import { setMandatoryFieldsFromSchema } from "./schema-utils.js";
 // Entry point: called when DOM is fully loaded
-document.addEventListener('DOMContentLoaded', () => {
- loadpage();
-  setupForm();
+document.addEventListener("DOMContentLoaded", () => {
+  loadpage();
   initializeTaggingFields();
   setupTables();
   setupDownload();
-  setupUI();
   initializeDynamicDropdowns();
   setMandatoryFieldsFromSchema();
-  
+  setupForm();
+  setupUI();
 });
